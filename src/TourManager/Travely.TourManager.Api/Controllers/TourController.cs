062f89e2450a6b4e69c42aa30c31df413fe0b2e9
Grigor Yeghiazaryan <grigor.yeghiazaryan26@gmail.com>
2021-01-16 01:52:24 +0400

    empty TourController added

---
+++ src/TourManager/Travely.TourManager.Api/Controllers/TourController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Travely.TourManager.Api.Controllers
+{
+    [Route("api/Tour")]
+    public class TourController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
