7ae4598c2ec2ffe999048b9871371f3a8ef4b854
Vachik Yeghiazaryan <vyeghiazaryan@outlook.com>
2021-01-18 23:10:43 +0400

    fixed dotnet versions of all class libraries, service port, root EP response message

--- src/PropertyManager/Infrastructure/Travely.PropertyManager.GrpcService/Startup.cs
+++ src/PropertyManager/Infrastructure/Travely.PropertyManager.GrpcService/Startup.cs
@@ -41,7 +41,7 @@ namespace Travely.PropertyManager.GrpcService
 
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                    await context.Response.WriteAsync("Property Manager gRPC service is up.");
                 });
             });
         }
