da20b1276c34361a7c1ff1a3e6647c756d1af43a
Erick <erick.araqelyan@gmail.com>
2021-01-19 01:23:46 +0400

    deleted grpc default created files

--- src/ClientManager/Travely.ClientManager.Service/Startup.cs
+++ src/ClientManager/Travely.ClientManager.Service/Startup.cs
@@ -32,7 +32,6 @@ namespace Travely.ClientManager.Service
             services.InstallRepositoryServices();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
