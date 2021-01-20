61dc261f18b0bc076ec2bf11e87c56666c96a88f
Ashot Petrosyan <ashotpet97@gmail.com>
2021-01-20 22:42:10 +0400

    Project layers setup

---
+++ src/SchedulerManager/Travely.SchedulerManager.API/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Travely.SchedulerManager.API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGet("/", async context => await context.Response.WriteAsync("Service running"));
+            });
+        }
+    }
+}
