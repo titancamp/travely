36757ee95aec2245304865071db44d132fe92ad4
Grigor <grigoryeghiazaryan26@gmail.com>
2021-02-08 20:21:10 +0400

    FluentValidation initial setup

---
+++ src/TourManager/TourManager.Service.Implementation/Validations/TourValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using TourManager.Service.Model;
+
+namespace TourManager.Service.Implementation.Validations
+{
+    public class TourValidator : AbstractValidator<Tour>
+    {
+        public TourValidator()
+        {
+            RuleFor(a => a.TourName)
+                .NotEmpty()
+                .MaximumLength(50);
+        }
+    }
+}
