a60b190d510add981c145c50f921bcfc33dd3bc1
Tadevos Stepanyan <57753967+TadevosStepanyan@users.noreply.github.com>
2021-02-10 15:36:56 +0400

    authorize-validation: add attribute for authentication validation

---
+++ src/Shared/IdentityClient/Types.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Travely.IdentityManager.IdentityService.Authorization
+{
+    [Flags]
+    public enum UserTypes
+    {
+        None = 0,
+
+        User = 1,
+        Admin = 1 << 1,
+
+        Both = 1 << 2,
+    }
+}
