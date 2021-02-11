66ae311bd43600b086ea0fd5ee04e6137abc49a5
Server <Server@SERVER-PC>
2021-02-11 23:49:40 +0400

    Added fields for property class

--- src/PropertyManager/Domain/Travely.PropertyManager.Domain.Entities/Property.cs
+++ src/PropertyManager/Domain/Travely.PropertyManager.Domain.Entities/Property.cs
@@ -5,8 +5,13 @@
         public int Id { get; set; }
 
         public string Name { get; set; }
-        public string Description { get; set; }
-
-
+        public float Stars { get; set; }
+        public string Address { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+        public string ContactName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string WebSite { get; set; }
     }
 }
--- src/PropertyManager/Infrastructure/Travely.PropertyManager.Data/EntityFramework/Configurations/PropertyConfiguration.cs
+++ src/PropertyManager/Infrastructure/Travely.PropertyManager.Data/EntityFramework/Configurations/PropertyConfiguration.cs
@@ -10,8 +10,13 @@ namespace Travely.PropertyManager.Data.EntityFramework.Configurations
         {
             builder.ToTable(nameof(Property))
                 .HasKey(e => e.Id);
+  
+            builder.Property(p => p.Name).IsRequired();
+            builder.Property(p => p.Longitude).HasPrecision(9,6); 
+            builder.Property(p => p.Latitude).HasPrecision(8,6);
+            builder.Property(p => p.Email).HasMaxLength(320);
+            builder.Property(p => p.Phone).HasMaxLength(15);
 
-            builder.Property(p => p.Name).HasMaxLength(100); 
         }
     }
 }
