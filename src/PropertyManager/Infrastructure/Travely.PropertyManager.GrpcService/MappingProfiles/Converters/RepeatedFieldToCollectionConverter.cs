89dc3da3995a038e02b56eebe5dd0fa66b6064dd
Shirak Gyunashyan <shirak.gyunashyan@digitain.com>
2021-02-12 16:53:56 +0400

    bugfix, mapper converter added

---
+++ src/PropertyManager/Infrastructure/Travely.PropertyManager.GrpcService/MappingProfiles/Converters/RepeatedFieldToCollectionConverter.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Google.Protobuf.Collections;
+using System.Collections.Generic;
+
+namespace Travely.PropertyManager.GrpcService.MappingProfiles.Converters
+{
+    internal class RepeatedFieldToCollectionConverter<TITemSource, TITemDest> : ITypeConverter<RepeatedField<TITemSource>, ICollection<TITemDest>>
+    {
+        public ICollection<TITemDest> Convert(RepeatedField<TITemSource> source, ICollection<TITemDest> destination, ResolutionContext context)
+        {
+            destination = destination ?? new List<TITemDest>();
+            foreach (var item in source)
+            {
+                destination.Add(context.Mapper.Map<TITemDest>(item));
+            }
+            return destination;
+        }
+    }
+}
--- src/PropertyManager/Infrastructure/Travely.PropertyManager.GrpcService/MappingProfiles/PropertyMappingProfile.cs
+++ src/PropertyManager/Infrastructure/Travely.PropertyManager.GrpcService/MappingProfiles/PropertyMappingProfile.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
+using Google.Protobuf.Collections;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using Travely.PropertyManager.Domain.Models.Commands;
 using Travely.PropertyManager.Domain.Models.Queries;
 using Travely.PropertyManager.Domain.Models.Responses;
 using Travely.PropertyManager.GrpcService.Contracts;
+using Travely.PropertyManager.GrpcService.MappingProfiles.Converters;
 
 namespace Travely.PropertyManager.GrpcService.MappingProfiles
 {
@@ -12,15 +15,22 @@ namespace Travely.PropertyManager.GrpcService.MappingProfiles
     {
         public PropertyMappingProfile()
         {
+            CreateMap(typeof(RepeatedField<>), typeof(ICollection<>)).ConvertUsing(typeof(RepeatedFieldToCollectionConverter<,>));
+
+
             CreateMap<AddPropertyRequest, AddPropertyCommand>()
                 .ForMember(dest => dest.Latitude, src => src.MapFrom(i => string.IsNullOrEmpty(i.Latitude) ? (decimal?)null : decimal.Parse(i.Latitude, CultureInfo.InvariantCulture)))
                 .ForMember(dest => dest.Longitude, src => src.MapFrom(i => string.IsNullOrEmpty(i.Longitude) ? (decimal?)null : decimal.Parse(i.Longitude, CultureInfo.InvariantCulture)));
-             
+
             CreateMap<GetPropertiesRequest, GetPropertiesQuery>();
 
             CreateMap<PropertyResponse, GetPropertiesResponse>()
                 .ForMember(dest => dest.Latitude, src => src.MapFrom(i => i.Latitude.ToString()))
                 .ForMember(dest => dest.Longitude, src => src.MapFrom(i => i.Longitude.ToString()));
+
+            CreateMap<Travely.PropertyManager.GrpcService.Contracts.OrderingBaseModel, Travely.PropertyManager.Domain.Entities.Base.OrderingBaseModel>();
+            CreateMap<Travely.PropertyManager.GrpcService.Contracts.FilteringBaseModel, Travely.PropertyManager.Domain.Entities.Base.FilteringBaseModel>();
+
         }
     }
 }
