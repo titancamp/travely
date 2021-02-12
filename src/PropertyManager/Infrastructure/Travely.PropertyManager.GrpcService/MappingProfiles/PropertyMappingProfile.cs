e7e9901302a8a5e375fa3ec26186d1ca25e4d3bc
Server <Server@SERVER-PC>
2021-02-12 16:14:55 +0400

    changes in proto file, and api service

--- src/PropertyManager/Infrastructure/Travely.PropertyManager.GrpcService/MappingProfiles/PropertyMappingProfile.cs
+++ src/PropertyManager/Infrastructure/Travely.PropertyManager.GrpcService/MappingProfiles/PropertyMappingProfile.cs
@@ -1,4 +1,10 @@
 ﻿using AutoMapper;
+using System;
+using System.Globalization;
+using Travely.PropertyManager.Domain.Models.Commands;
+using Travely.PropertyManager.Domain.Models.Queries;
+using Travely.PropertyManager.Domain.Models.Responses;
+using Travely.PropertyManager.GrpcService.Contracts;
 
 namespace Travely.PropertyManager.GrpcService.MappingProfiles
 {
@@ -6,7 +12,15 @@ namespace Travely.PropertyManager.GrpcService.MappingProfiles
     {
         public PropertyMappingProfile()
         {
+            CreateMap<AddPropertyRequest, AddPropertyCommand>()
+                .ForMember(dest => dest.Latitude, src => src.MapFrom(i => string.IsNullOrEmpty(i.Latitude) ? (decimal?)null : decimal.Parse(i.Latitude, CultureInfo.InvariantCulture)))
+                .ForMember(dest => dest.Longitude, src => src.MapFrom(i => string.IsNullOrEmpty(i.Longitude) ? (decimal?)null : decimal.Parse(i.Longitude, CultureInfo.InvariantCulture)));
+             
+            CreateMap<GetPropertiesRequest, GetPropertiesQuery>();
 
+            CreateMap<PropertyResponse, GetPropertiesResponse>()
+                .ForMember(dest => dest.Latitude, src => src.MapFrom(i => i.Latitude.ToString()))
+                .ForMember(dest => dest.Longitude, src => src.MapFrom(i => i.Longitude.ToString()));
         }
     }
 }
