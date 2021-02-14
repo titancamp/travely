ddec2218c9907ee73b0e4e9ea7a2eb1ffc5dd041
Gevorg Simonyan <63793616+simonyangevorg@users.noreply.github.com>
2021-02-14 17:28:42 +0400

    Service manager/issue 11 create grpc services for activity crud (#94)
    * Created proto file vor activity and inital create activity service
    
    includes Activity model, activity type model, and basic reposnce type
    
    * grpc services for crud, business logic layer in service, automapper
    
    Added grpc services for crud operations
    created business logic layer in Service project ->  Management
    added automapper to project - > will implement when we have models from DL
    
    * Update ActivityService.cs
    
    removed unused code
    
    * fixed few issues related to naming and code styling, removed unnecessary code
    
    * added RootNamespace in csproj, did some changes on activity manager return types
    
    * typo fixed for ActivityResponse
    
    * changed activityId in DeleteActivityRequest long? -> long
    
    * injected service null check
    
    * Update src/ServiceManager/Travely.ServiceManager.Service/Travely.ServiceManager.Service.csproj
    
    Co-authored-by: Arthur Tadevosyan <spone7@gmail.com>
    
    Co-authored-by: Gevorg Simonyan <gevorg.simonyan@praemium.com>
    Co-authored-by: Arthur Tadevosyan <spone7@gmail.com>

---
+++ src/ServiceManager/Travely.ServiceManager.Service/Managers/ActivityManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travely.ServiceManager.Service.Managers
+{
+    public class ActivityManager : IActivityManager
+    {
+        public Task<Activity> CreateActivityAsync(Activity activity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Activity>> GetActivitiesAsync(int agencyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActivityResponse> DeleteActivityAsync(long activityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Activity> EditActivityAsync(Activity activity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ServiceManager/Travely.ServiceManager.Service/Mappers/ActivityProfile.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace Travely.ServiceManager.Service.Mappers
+{
+    public class ActivityProfile : Profile
+    {
+
+    }
+}
