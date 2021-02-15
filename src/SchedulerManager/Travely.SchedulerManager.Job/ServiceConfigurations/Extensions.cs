aa5f191ee8c39f6d91992a3c2209a2c7cc4a4f5b
Ashot Petrosyan <ashotpet97@gmail.com>
2021-02-15 22:18:01 +0400

    merge

--- src/SchedulerManager/Travely.SchedulerManager.Job/ServiceConfigurations/Extensions.cs
+++ src/SchedulerManager/Travely.SchedulerManager.Job/ServiceConfigurations/Extensions.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using Travely.SchedulerManager.Abstraction;
 
 namespace Travely.SchedulerManager.Job
 {
