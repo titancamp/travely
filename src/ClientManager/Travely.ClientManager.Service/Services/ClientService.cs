902ef099927596637f49322c2fc396f0b839490b
Vardan Ghazaryan <vardan.s.ghazaryan@digitain.com>
2021-02-17 16:46:37 +0400

    Addes exception handling

--- src/ClientManager/Travely.ClientManager.Service/Services/ClientService.cs
+++ src/ClientManager/Travely.ClientManager.Service/Services/ClientService.cs
@@ -10,175 +10,146 @@ using System.Threading.Tasks;
 using Travely.ClientManager.Abstraction.Abstraction.Repository;
 using Travely.ClientManager.Abstraction.Entity;
 using Travely.ClientManager.Service.Protos;
+using Travely.Services.Common.CustomExceptions;
 
 namespace Travely.ClientManager.Service.Services
 {
-    public class ClientService : ClientProtoService.ClientProtoServiceBase
-    {
-        private readonly ITouristRepository _touristRepository;
-        private readonly IMapper _mapper;
-        private readonly ILogger<ClientService> _logger;
-        public ClientService(ILogger<ClientService> logger, ITouristRepository turistRepository, IMapper mapper)
-        {
-            _logger = logger;
-            _touristRepository = turistRepository;
-            _mapper = mapper;
-        }
+	public class ClientService : ClientProtoService.ClientProtoServiceBase
+	{
+		private readonly ITouristRepository _turistRepository;
+		private readonly IMapper _mapper;
+		private readonly ILogger<ClientService> _logger;
+		public ClientService(ILogger<ClientService> logger, ITouristRepository turistRepository, IMapper mapper)
+		{
+			_logger = logger;
+			_turistRepository = turistRepository;
+			_mapper = mapper;
+		}
 
-        #region GET
-        public override async Task<ClientModel> GetClient(GetClientRequest request,
-                                                                ServerCallContext context)
-        {
-            try
-            {
-                Tourist client = await _touristRepository.GetNoTracking(x => x.Id == request.Id).FirstOrDefaultAsync();
+		#region GET
+		public override async Task<ClientModel> GetClient(GetClientRequest request,
+																ServerCallContext context)
+		{
+			Tourist client = await _turistRepository.GetNoTracking(x => x.Id == request.Id).FirstOrDefaultAsync();
 
-                if (client == null)
-                {
-                    throw new RpcException(new Status(StatusCode.NotFound, $"Client with ID={request.Id} is not found."));
-                }
+			if (client == null)
+			{
+				throw new NotFoundException(nameof(Tourist), request.Id);
+			}
 
-                ClientModel clientModel = _mapper.Map<ClientModel>(client);
+			ClientModel clientModel = _mapper.Map<ClientModel>(client);
 
-                return clientModel;
-            }
-            catch (Exception ex)
-            {
-                throw new RpcException(new Status(StatusCode.NotFound, $"Client with ID={request.Id} is not found."));
-            }
+			return clientModel;
+		}
+
+		public override async Task<ClientWithPreferencesModel> GetClientWithPreferences(GetClientRequest request,
+																ServerCallContext context)
+		{
+			Tourist client = await _turistRepository.GetNoTracking(x => x.Id == request.Id, "Preferences").FirstOrDefaultAsync();
+
+			if (client == null)
+			{
+				throw new NotFoundException(nameof(Tourist), request.Id);
+			}
+
+			ClientWithPreferencesModel clientPreferencesModel = _mapper.Map<ClientWithPreferencesModel>(client);
+
+			return clientPreferencesModel;
+		}
+
+		public override async Task GetAllClients(GetAllClientsRequest request,
+													IServerStreamWriter<ClientModel> responseStream,
+													ServerCallContext context)
+		{
+			List<Tourist> clientList = await _turistRepository.GetNoTracking().ToListAsync();
+
+			foreach (var client in clientList)
+			{
+				ClientModel clientModel = _mapper.Map<ClientModel>(client);
+
+				await responseStream.WriteAsync(clientModel);
+			}
+		}
+		#endregion
+
+		#region CREATE
+		public override async Task<ClientModel> CreateClient(CreateClientRequest request, ServerCallContext context)
+		{
+			Tourist client = _mapper.Map<Tourist>(request.Client);
+			client.Id = default;
+
+			_turistRepository.Add(client);
+			await _turistRepository.SaveChangesAsync();
+
+			var clientModel = request.Client;
+			return clientModel;
+		}
+
+		public override async Task<AddRangeClientReponse> AddRangeClient(IAsyncStreamReader<ClientModel> requestStream, ServerCallContext context)
+		{
+			while (await requestStream.MoveNext())
+			{
+				Tourist client = _mapper.Map<Tourist>(requestStream.Current);
+				_turistRepository.Add(client);
+			}
+
+			var insertCount = await _turistRepository.SaveChangesAsync();
+
+			var response = new AddRangeClientReponse
+			{
+				Success = insertCount > 0,
+				InsertCount = insertCount
+			};
+
+			return response;
+		}
+
+		#endregion
+
+		#region UPDATE
+
+		public override async Task<ClientModel> UpdateClient(UpdateClientRequest request, ServerCallContext context)
+		{
+			Tourist client = _mapper.Map<Tourist>(request.Client);
+
+			bool isExist = await _turistRepository.Get(x => x.Id == request.Client.Id).AnyAsync();
+
+			if (!isExist)
+			{
+				throw new NotFoundException(nameof(Tourist), request.Client.Id);
+			}
 
 
-        }
+			_turistRepository.Update(client);
+			await _turistRepository.SaveChangesAsync();
 
-        public override async Task<ClientWithPreferencesModel> GetClientWithPreferences(GetClientRequest request,
-                                                                ServerCallContext context)
-        {
-            try
-            {
-                Tourist client = await _touristRepository.GetNoTracking(x => x.Id == request.Id, "Preferences").FirstOrDefaultAsync();
+			ClientModel clientModel = _mapper.Map<ClientModel>(client);
+			return clientModel;
+		}
 
-                if (client == null)
-                {
-                    throw new RpcException(new Status(StatusCode.NotFound, $"Client with ID={request.Id} is not found."));
-                }
+		#endregion
 
-                ClientWithPreferencesModel clientPreferencesModel = _mapper.Map<ClientWithPreferencesModel>(client);
-                
-                return clientPreferencesModel;
-            }
-            catch (Exception ex)
-            {
-                throw new RpcException(new Status(StatusCode.NotFound, $"Client with ID={request.Id} is not found."));
-            }
-        }
+		#region DELETE
+		public override async Task<DeleteClientResponse> DeleteClient(DeleteClientRequest request, ServerCallContext context)
+		{
+			Tourist client = await _turistRepository.Get(x => x.Id == request.Id).FirstOrDefaultAsync();
+			if (client == null)
+			{
+				throw new NotFoundException(nameof(Tourist), request.Id);
+			}
 
-        public override async Task GetAllClients(GetAllClientsRequest request,
-                                                    IServerStreamWriter<ClientModel> responseStream,
-                                                    ServerCallContext context)
-        {
-            List<Tourist> clientList = await _touristRepository.GetNoTracking().ToListAsync();
+			_turistRepository.Delete(client);
 
-            foreach (var client in clientList)
-            {
-                try
-                {
-                    ClientModel clientModel = _mapper.Map<ClientModel>(client);
+			var deleteCount = await _turistRepository.SaveChangesAsync();
 
-                    await responseStream.WriteAsync(clientModel);
-                }
-                catch (Exception ex)
-                {
+			var response = new DeleteClientResponse
+			{
+				Success = deleteCount > 0
+			};
 
-                    throw;
-                }
-            }
-        }
-        #endregion
+			return response;
+		}
+		#endregion
 
-        #region CREATE
-        public override async Task<ClientModel> CreateClient(CreateClientRequest request, ServerCallContext context)
-        {
-            Tourist client = _mapper.Map<Tourist>(request.Client);
-            client.Id = default;
-
-            _touristRepository.Add(client);
-            await _touristRepository.SaveChangesAsync();
-
-            var clientModel = request.Client;
-            return clientModel;
-        }
-
-        public override async Task<AddRangeClientReponse> AddRangeClient(IAsyncStreamReader<ClientModel> requestStream, ServerCallContext context)
-        {
-            while (await requestStream.MoveNext())
-            {
-                Tourist client = _mapper.Map<Tourist>(requestStream.Current);
-                _touristRepository.Add(client);
-            }
-
-            var insertCount = await _touristRepository.SaveChangesAsync();
-
-            var response = new AddRangeClientReponse
-            {
-                Success = insertCount > 0,
-                InsertCount = insertCount
-            };
-
-            return response;
-        }
-
-        #endregion
-
-        #region UPDATE
-
-        public override async Task<ClientModel> UpdateClient(UpdateClientRequest request, ServerCallContext context)
-        {
-            Tourist client = _mapper.Map<Tourist>(request.Client);
-
-            bool isExist = await _touristRepository.Get(x=>x.Id == request.Client.Id).AnyAsync();
-
-            if (!isExist)
-            {
-                throw new RpcException(new Status(StatusCode.NotFound, $"Client with ID={client.Id} is not found."));
-            }
-
-            try
-            {
-                _touristRepository.Update(client);
-                await _touristRepository.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                throw;
-            }
-
-            ClientModel clientModel = _mapper.Map<ClientModel>(client);
-            return clientModel;
-        }
-
-        #endregion
-
-        #region DELETE
-        public override async Task<DeleteClientResponse> DeleteClient(DeleteClientRequest request, ServerCallContext context)
-        {
-            Tourist client = await _touristRepository.Get(x=>x.Id == request.Id).FirstOrDefaultAsync();
-            if (client == null)
-            {
-                throw new RpcException(new Status(StatusCode.NotFound, $"Cleient with ID={request.Id} is not found."));
-            }
-
-            _touristRepository.Delete(client);
-
-            var deleteCount = await _touristRepository.SaveChangesAsync();
-
-            var response = new DeleteClientResponse
-            {
-                Success = deleteCount > 0
-            };
-
-            return response;
-        }
-        #endregion
-
-    }
+	}
 }
