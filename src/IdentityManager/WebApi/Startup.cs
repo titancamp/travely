61e1b1e8c15b17730f68d909a66a143aaf219b5d
erick20 <erick.araqelyan@gmail.com>
2021-02-17 19:17:56 +0400

    dbcontext startup config

--- src/IdentityManager/WebApi/Startup.cs
+++ src/IdentityManager/WebApi/Startup.cs
@@ -31,6 +31,7 @@ namespace IdentityManager.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureSqlContext(Configuration);
             services.InstallIdentityServices();
 
             services.InstallRepositoryServices();
