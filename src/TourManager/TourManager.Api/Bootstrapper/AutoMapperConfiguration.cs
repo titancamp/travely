06231f6a6d3c9d3db5594260e0845dadcffb582f
Grigor <grigoryeghiazaryan26@gmail.com>
2021-02-19 20:15:14 +0400

    move configurations to bootstrapper

---
+++ src/TourManager/TourManager.Api/Bootstrapper/AutoMapperConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TourManager.Api.Bootstrapper
+{
+    public static class AutoMapperConfiguration
+    {
+        public static IServiceCollection AddAutoMapper(this IServiceCollection services)
+        {
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingConfig());
+            });
+
+            services.AddSingleton(mapperConfig.CreateMapper());
+
+            return services;
+        }
+    }
+}
---
+++ src/TourManager/TourManager.Api/Bootstrapper/TourManagerDIConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TourManager.Api.Bootstrapper
+{
+    public static class TourManagerDIConfiguration
+    {
+        public static IServiceCollection AddTourManagerServices(this IServiceCollection services)
+        {
+            // configure DI for application services
+
+            return services;
+        }
+    }
+}
