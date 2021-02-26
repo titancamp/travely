66d08800510120e92c628e9c859f60eb05bd1d5b
Lusine_Hovsepyan <lusine@hovsepyan.am>
2021-02-26 17:54:29 +0400

    Issue-124 - BL functions added, Currency set to non-nullable, Service changed

--- src/ServiceManager/Travely.ServiceManager.Abstraction/Interfaces/Repositories/IActivityRepository.cs
+++ src/ServiceManager/Travely.ServiceManager.Abstraction/Interfaces/Repositories/IActivityRepository.cs
@@ -1,8 +1,12 @@
-﻿using Travely.ServiceManager.Abstraction.Models.Db;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Travely.ServiceManager.Abstraction.Models.Db;
 
 namespace Travely.ServiceManager.Abstraction.Interfaces
 {
     public interface IActivityRepository : IRepository<Activity>
     {
+        Task<List<Activity>> GetAllActivitiesByAgencyIdAsync(long agencyId);
+        Task<Activity> GetActivityByUniqueKeysAsync(long agencyId, string activityName, long activityTypeId);
     }
 }
