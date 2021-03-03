c728f941dae28f94f79331b5546cfba9e89cb354
Gor Rustamyan <56830600+GRustamyan-ST@users.noreply.github.com>
2021-03-03 19:21:12 +0400

    Mocked identity client. (#141)
    * feat: identity-manager client
    
    * feat: added usage example
    
    * fix: readme.md format fix.

---
+++ src/Commons/IdentityClient/Common/Common.cs
@@ -0,0 +1,12 @@
+﻿namespace Travely.IdentityClient
+{
+    //
+    // Summary:
+    //     class for user Roles for Authorize attribute
+    //
+    public static class UserTypes
+    {
+        public const string User = "User";
+        public const string Admin = "Admin";
+    }
+}
