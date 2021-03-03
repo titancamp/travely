bcae48514310dc56aa08330184122ad30209b3cf
Karo Tavrizyan <karo.tavrizyan.96@gmail.com>
2021-03-03 10:59:42 -0600

    Adding ScheduleJobRepository

---
+++ src/SchedulerManager/Travely.SchedulerManager.Repository/Implementation/ScheduleJobRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travely.SchedulerManager.Repository.Entities;
+
+namespace Travely.SchedulerManager.Repository.Implementation
+{
+    public class ScheduleJobRepository : Repository<ScheduleJob>, IScheduleJobRepository
+    {
+        public ScheduleJobRepository(SchedulerDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<string>> GetJobIdAsync(long scheduleInfoId)
+        {
+            var jobList = await GetListAsync(o => o.ScheduleInfoId == scheduleInfoId);
+            return jobList.Select(o => o.JobId);
+        }
+    }
+}
