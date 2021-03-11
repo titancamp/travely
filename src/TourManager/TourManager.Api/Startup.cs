118c0ca3dd44b199c195e248cd217a68279e4e92
Grigor <grigoryeghiazaryan26@gmail.com>
2021-03-11 22:20:00 +0400

    fixes after merge

--- src/TourManager/TourManager.Api/Startup.cs
+++ src/TourManager/TourManager.Api/Startup.cs
@@ -5,15 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using TourManager.Repository.EfCore.Context;
 using TourManager.Clients.Abstraction.ServiceManager;
 using TourManager.Clients.Implementation.ServiceManager;
 using TourManager.Common.Settings;
 using TourManager.Clients.Abstraction.Settings;
 using TourManager.Clients.Implementation.Settings;
-
-using System.Reflection;
 using TourManager.Api.Bootstrapper;
 using TourManager.Service.Model;
 
@@ -41,15 +37,10 @@ namespace TourManager.Api
                 config.DefaultApiVersion = new ApiVersion(1, 0);
                 config.AssumeDefaultVersionWhenUnspecified = true;
             });
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TourManager.Api", Version = "v1" });
-            });
 
             services.AddScoped<IServiceManagerClient, ServiceManagerClient>();
             services.AddScoped<IServiceSettingsProvider, ServiceSettingsProvider>();
             services.Configure<GrpcServiceSettings>(Configuration.GetSection("GrpcServiceSettings"));
-
 
             services
                 .AddSqlServer(Configuration)
