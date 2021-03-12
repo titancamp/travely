c149ddf5083a55a26f8bc4ad51a11dfdf35602fc
Grigor <grigoryeghiazaryan26@gmail.com>
2021-03-12 12:45:33 +0400

    Tour manager (#169)

--- src/TourManager/TourManager.Api/Startup.cs
+++ src/TourManager/TourManager.Api/Startup.cs
@@ -5,15 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using TourManager.Repository.EfCore.Context;
 using TourManager.Clients.Abstraction.ServiceManager;
 using TourManager.Clients.Implementation.ServiceManager;
 using TourManager.Common.Settings;
 using TourManager.Clients.Abstraction.Settings;
 using TourManager.Clients.Implementation.Settings;
-
-using System.Reflection;
 using TourManager.Api.Bootstrapper;
 using TourManager.Service.Model;
 
@@ -41,21 +37,16 @@ namespace TourManager.Api
                 config.DefaultApiVersion = new ApiVersion(1, 0);
                 config.AssumeDefaultVersionWhenUnspecified = true;
             });
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TourManager.Api", Version = "v1" });
-            });
 
-            services.AddScoped<IServiceManagerClient, ServiceManagerClient>();
-            services.AddScoped<IServiceSettingsProvider, ServiceSettingsProvider>();
             services.Configure<GrpcServiceSettings>(Configuration.GetSection("GrpcServiceSettings"));
-
 
             services
                 .AddSqlServer(Configuration)
                 .AddAutoMapper(typeof(Startup))
                 .AddSwagger()
-                .AddTourManagerServices();
+                .AddTourManagerServices()
+                .AddTourManagerRepositories()
+                .AddTourClientServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
