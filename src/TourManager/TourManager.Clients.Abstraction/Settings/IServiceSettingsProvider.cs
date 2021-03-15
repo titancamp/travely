60c12a9435cfe25d12e1ed7520dae52cc3922c44
Vachik Yeghiazaryan <vyeghiazaryan@outlook.com>
2021-03-15 21:11:14 +0400

    removed redundant usings

--- src/TourManager/TourManager.Clients.Abstraction/Settings/IServiceSettingsProvider.cs
+++ src/TourManager/TourManager.Clients.Abstraction/Settings/IServiceSettingsProvider.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TourManager.Clients.Abstraction.Settings
+﻿namespace TourManager.Clients.Abstraction.Settings
 {
     public interface IServiceSettingsProvider
     {
