d9d992b67b4886f69e20ec01e541550f9111879f
Grigor <grigoryeghiazaryan26@gmail.com>
2021-03-27 12:29:42 +0400

    Tourmanager/merge from master (#211)

---
+++ src/TourManager/TourManager.Common/Clients/PropertyManager/FileModel.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace TourManager.Common.Clients.PropertyManager
+{
+    public class FileModel
+    {
+        public string Name { get; set; }
+
+        public string ContentType { get; set; }
+
+        public Stream Stream { get; set; }
+    }
+}
