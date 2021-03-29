01ca29db3dcd450a14d36c94cd9d3c4c965ddf39
Tatev Asatryan <tatev.asatryan@sololearn.com>
2021-03-29 20:30:52 +0400

    Hangfire Authorization

---
+++ src/SchedulerManager/Travely.SchedulerManager.Job/ServiceConfigurations/HangfireAuthorizationFilter.cs
@@ -0,0 +1,14 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Dashboard;
+
+namespace Travely.SchedulerManager.Job
+{
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            var httpContext = context.GetHttpContext();
+            return httpContext.User.Identity.IsAuthenticated && httpContext.User.IsInRole("Admin"); 
+        }
+    }
+}
