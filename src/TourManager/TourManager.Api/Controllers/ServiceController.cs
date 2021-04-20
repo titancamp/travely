1755aaf8bd5391b6fb42e8b5a02919fd5ffd611b
Lusine Hovsepyan <lusine@hovsepyan.am>
2021-04-20 13:40:50 +0400

    Issue- Auth Implementation - Authentication and role parts added (#204)
    * Issue- Auth Implementation - Authentication and role parts added
    
    * Auth_implementation - Identity UserExtension GetTravelyUserInfo() function is called to get identity userinfo

--- src/TourManager/TourManager.Api/Controllers/ServiceController.cs
+++ src/TourManager/TourManager.Api/Controllers/ServiceController.cs
@@ -3,33 +3,43 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using TourManager.Clients.Abstraction.ServiceManager;
 using TourManager.Common.Clients;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Travely.IdentityClient.Authorization;
 
 namespace TourManager.Api.Controllers
 {
     [ApiController]
     [ApiVersion("1.0")]
     [Route("api/v{version:apiVersion}/[controller]")]
+    [Authorize]
     public class ServiceController : Controller
     {
         private readonly IServiceManagerClient _serviceManagerClient;
+        private readonly long _agencyId;
         public ServiceController(IServiceManagerClient serviceManagerClient)
         {
             _serviceManagerClient = serviceManagerClient;
+            var userInfo = HttpContext.GetTravelyUserInfo();
+            if (userInfo is not null) { _agencyId = userInfo.AgencyId; }
         }
 
-        // GET api/<ServiceController>/5
-        [HttpGet("{agencyId}")]
-        public async Task<IEnumerable<Activity>> Get(long agencyId)
+        // GET api/<ServiceController>
+        [HttpGet]
+        public async Task<IEnumerable<Activity>> Get()
         {
-            var result = await _serviceManagerClient.GetActivitiesAsync(agencyId);
+            var result = await _serviceManagerClient.GetActivitiesAsync(_agencyId);
 
             return result;
         }
 
         // POST api/<ServiceController>
         [HttpPost]
+        [Authorize(Roles = UserRoles.Admin)]
         public async Task<ActivityResponse> Post([FromBody] Activity activity)
         {
+            if (activity.Type != null) { activity.Type.AgencyId = _agencyId; }
+
             var result = await _serviceManagerClient.CreateActivityAsync(activity);
 
             return result;
@@ -37,8 +47,11 @@ namespace TourManager.Api.Controllers
 
         // PUT api/<ServiceController>
         [HttpPut]
+        [Authorize(Roles = UserRoles.Admin)]
         public async Task<ActivityResponse> Put([FromBody] Activity activity)
         {
+            if (activity.Type != null) { activity.Type.AgencyId = _agencyId; }
+
             var result = await _serviceManagerClient.EditActivityAsync(activity);
 
             return result;
@@ -46,6 +59,7 @@ namespace TourManager.Api.Controllers
 
         // DELETE api/<ServiceController>/5
         [HttpDelete("{id}")]
+        [Authorize(Roles = UserRoles.Admin)]
         public async Task<ActivityResponse> Delete(int id)
         {
             var result = await _serviceManagerClient.DeleteActivityAsync(id);
@@ -53,11 +67,11 @@ namespace TourManager.Api.Controllers
             return result;
         }
 
-        // GET api/<ServiceController>/5/activitytype/cafe
-        [HttpGet("{agencyId}/activitytype/{activityTypeName}")]
-        public async Task<IEnumerable<ActivityType>> GetSearchedActivityTypes(long agencyId, string activityTypeName)
+        // GET api/<ServiceController>/activitytype/cafe
+        [HttpGet("activitytype/{activityTypeName}")]
+        public async Task<IEnumerable<ActivityType>> GetSearchedActivityTypes(string activityTypeName)
         {
-            var result = await _serviceManagerClient.SearchActivityTypesAsync(agencyId, activityTypeName);
+            var result = await _serviceManagerClient.SearchActivityTypesAsync(_agencyId, activityTypeName);
 
             return result;
         }
