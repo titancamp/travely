7a46504c588a4a1df7b29ffe08a2c352709ef52b
Ruben Basentsyan <basentsyanruben@outlook.com>
2022-01-08 00:19:22 +0400

    TS-14 - Added the initial partial class for Tour and all the classes related to it.

---
+++ src/Shared/TourModels/Tour/Group.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TourModels.Tour
+{
+    public class Group
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int NumberOfParticipants { get; set; }
+        public int NumberOfChildren { get; set; }
+        public string Country { get; set; }
+        public string Language { get; set; }
+        public List<string> Preferences { get; set; }
+        public List<string> Attachments { get; set; }
+    }
+}
--- src/Shared/TourModels/Tour/TMTour.cs
+++ src/Shared/TourModels/Tour/TMTour.cs
@@ -1,7 +1,24 @@
+using System;
+using System.Collections.Generic;
+using TourModels.Tour;
+
 namespace TourEntities.Tour
 {
     public partial class Tour
     {
-        
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public TimeSpan StartDate { get; set; }
+        public TimeSpan EndDate { get; set; }
+        public string PartnerName { get; set; }
+        public Group Group { get; set; }
+        public List<Participant> Participants { get; set; }
+        public Arrival Arrival { get; set; }
+        public Departure Departure { get; set; }
+        public string Status { get; set; }
+        public string Notes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastEditedAt { get; set; }
     }
 }
