b62fb09e40a98d3064ee308de52d6c74e62312e3
hayksmnPA <hayk.s@picsart.com>
2022-01-11 21:21:30 +0400

    added shared models for tour

---
+++ src/Shared/TourModels/Tour/Participant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TourModels.Tour
+{
+    public class Participant
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int GroupId { get; set; }
+        public string Sex { get; set; }
+        public int Age { get; set; }
+        public string PassportNumber { get; set; }
+        public string ContactNumber { get; set; }
+        public string ContactEmail { get; set; }
+        public string SpecialPreferences { get; set; }
+        public bool AllInclusive { get; set; }
+    }
+}
--- src/Shared/TourModels/Tour/TMTour.cs
+++ src/Shared/TourModels/Tour/TMTour.cs
@@ -1,7 +1,23 @@
-namespace TourEntities.Tour
+using System;
+using System.Collections.Generic;
+
+namespace TourModels.Tour
 {
     public partial class Tour
     {
-        
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public TimeSpan StartDate { get; set; }
+        public TimeSpan EndDate { get; set; }
+        public string PartnerName { get; set; }
+        public Group Group { get; set; }
+        public List<Participant> Participants { get; set; }
+        public Arrival Arrival { get; set; }
+        public Departure Departure { get; set; }
+        public string Status { get; set; }
+        public string Notes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastEditedAt { get; set; }
     }
 }
