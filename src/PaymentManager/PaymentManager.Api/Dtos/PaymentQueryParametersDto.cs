059cc8f62bf6c3ae0f5b5449d358f109ae544348
razmikTovmas <razmik.tovmas@gmail.com>
2022-02-17 11:38:23 +0400

    Add validation in query DTO

--- src/PaymentManager/PaymentManager.Api/Dtos/PaymentQueryParametersDto.cs
+++ src/PaymentManager/PaymentManager.Api/Dtos/PaymentQueryParametersDto.cs
@@ -1,9 +1,13 @@
-﻿namespace PaymentManager.Api.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentManager.Api.Dtos
 {
     public class PaymentQueryParametersDto
     {
+        [Range(1, int.MaxValue, ErrorMessage = "Value for {0} must be bigger than {1}")]
         public int Index { get; set; } = 1;
-        public int Size { get; set; } = 10;
+        [Range(10, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Size { get; set; } = 20;
         public string OrderBy { get; set; }
         public string Search { get; set; }
     }
